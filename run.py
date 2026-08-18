20694f6460ec1e5437f12b278a1801681ec5d83d
Lopatushka <helena.lopatukhina@gmail.com>
2026-08-18 13:55:19 +0200

    split channel

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from ij import IJ, WindowManager
 from ij.gui import GenericDialog
 from ij.plugin.frame import RoiManager
 from ij.gui import ShapeRoi
-from ij.plugin.filter import BackgroundSubtracter
+from ij.plugin import ChannelSplitter
 from ij.measure import Measurements, ResultsTable
 from ij.process import ImageStatistics
 from ij.gui import NonBlockingGenericDialog
@@ -23,7 +23,20 @@ def close_image(imp):
         imp.close()
            
 def img_analysis(imp):
-    pass
+    imp_name = imp.getTitle().split(".")[0] # get image name without extension
+    
+    # Split channels
+    channels = ChannelSplitter.split(imp)
+    if len(channels) != 3:
+        IJ.log("Image {} does not have 3 channels!".format(imp_name))
+        return
+    
+    c1 = channels[0] # DAPI channel
+    c2 = channels[1] # MEARGUREMENT 1
+    c3 = channels[2] # MEARGUREMENT 2
+
+        
+        
 
 # ---------------------------------
 # MAIN FUNCTION
